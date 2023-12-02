bac49b04780290f350e0bed88b92f719366644c5
bekturbeknelox <bek.toktonazarov1@gmail.com>
2023-12-02 19:56:42 +0600

    first commit

---
+++ uzbek1-main/SecretSanta/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path('signup', views.signup),
+    path('login', views.login),
+    path('test_token', views.test_token),
+    path("users/", include("users.urls")),
+    path('',include('users.urls')),
+]
